5f17fffa2b31f8183fb8030495f63b23371dac56
ralam <rehan.alam@gmail.com>
2015-08-25 09:36:47 -0700

    store snake directions

--- js/snake.js
+++ js/snake.js
@@ -1,8 +1,20 @@
 (function () {
   var Game = window.Game = window.Game || {};
 
+  var Coord = Game.Coord = function (x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
   var Snake = Game.Snake = function (board) {
     this.dir = "N";
     this.board = board;
+  };
+
+  Snake.DIRS = {
+    "N": new Coord(-1, 0),
+    "E": new Coord(0, 1),
+    "S": new Coord(1, 0),
+    "W": new Coord(0, -1)
   }
 })
